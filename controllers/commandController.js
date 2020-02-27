ee57628ec66873e5d7c19da0cba03c896b354d98
Ujwal Basnet <ujwalbasnet1@gmail.com>
2020-02-27 08:03:51 +0545

    .

--- controllers/commandController.js
+++ controllers/commandController.js
@@ -1,13 +1,23 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+var led = false;
+
+board.on("ready", function() {
+  // Create a standard `led` component instance
+  led = new five.Led(13);
+});
+
 exports.parseCommands = async (req, res) => {
   try {
     const command = req.params.command;
 
     if (command === "turn on lights") {
       console.log("turning on lights");
-      //TODO: Implement turn on lights logic
+      if (led) led.on();
     } else if (command === "turn off lights") {
       console.log("turning off lights");
-      //TODO: Implement turn off lights logic
+      if (led) led.off();
     }
 
     res.redirect("back");
